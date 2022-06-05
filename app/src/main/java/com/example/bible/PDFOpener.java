1ee45e1fbddc9bdc237f85b3bb1e35632ff2f20d
Tsion <tsimitikneh@gmail.com>
2022-06-05 10:05:47 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/bible/PDFOpener.java
@@ -0,0 +1,14 @@
+package com.example.bible;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PDFOpener extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdfopener);
+    }
+}
